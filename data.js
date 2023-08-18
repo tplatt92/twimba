9ab50b2af9ad1fcbaebdd055c3152760a3da1450
Tom Platt <thomasjplatt0107@gmail.com>
2023-08-18 11:31:54 +0100

    fixed image sourcing

--- data.js
+++ data.js
@@ -13,7 +13,7 @@ export const tweetsData = [
     },    
     {
         handle: `@Elon ✅`,
-        profilePic: `images/musk.png`,
+        profilePic: `/musk.png`,
         likes: 6500,
         retweets: 234,
         tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
